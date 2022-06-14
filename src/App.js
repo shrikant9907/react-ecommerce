acf7be08585fcc88f5e260ff86ddca50ba1e8fb3
Shrikant Yadav <shrikant9907@gmail.com>
2022-06-14 11:35:10 +0530

    index.html and assets

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 import React from 'react'; 
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import BsNavbar from './components/navbar/Navbar';
+import Header from './layouts/Header/Header';
 import { TopMenusRoutes } from './_config/TopMenus';
 
 const App = () => {
   return (
     <Router>
 
-      <BsNavbar brandname="React E-Commerce Logo" logotype="text" logosrc="" />
+      {/* <BsNavbar brandname="React E-Commerce Logo" logotype="text" logosrc="" /> */}
+      <Header />
       
       <Routes>
         {/* Top Menu Routes Configuration is in _config/TopMenus.js */}
---
+++ src/layouts/Header/Header.js
@@ -0,0 +1,12 @@
+import './Header.scss';
+import React from 'react';
+  
+const Header = (props) => {
+  return (
+    <>
+      Header
+    </>
+  )
+}
+
+export default Header;
